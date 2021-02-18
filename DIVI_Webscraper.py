2c58e58e746877524dc8355e827707d947de24ce
thecarguy97 <filip.boshkov@gmail.com>
2021-02-18 01:31:37 +0100

    pre loop 2

--- DIVI_Webscraper.py
+++ DIVI_Webscraper.py
@@ -4,9 +4,12 @@ import pathlib
 from selenium.webdriver import Chrome
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-#PATH = "C:/WebDriver/bin"
+
+# PATH = "C:/WebDriver/bin"
 currpath = pathlib.Path().absolute()
 register_path = str(currpath) + '\Register'
+start_date = input("Enter start date: format (y-m-d)")
+assert (type(start_date) == str)
 
 driver = webdriver.Chrome('C:/WebDriver/bin/chromedriver.exe')
 # open a given link, maximize window and accept cookies
@@ -17,24 +20,53 @@ cookies_link.click()
 # refresh window because of cookies pop-up hindering clickable area
 driver.refresh()
 
-#driver.execute_script("window.scrollBy(0,600)","")
+# driver.execute_script("window.scrollBy(0,600)","")
 driver.implicitly_wait(5)
+
+date = start_date
+
 start_link = driver.find_element_by_link_text('Start')
-reg_link = 'DIVI-Intensivregister_2020-05-01_09-15'
+back_link = driver.find_element_by_link_text('Zurück')
+# intensive_register = ""
 
-reg_path='//a[contains(@aria-label, "DIVI-Intensivregister_2021-01-01" )]'
-intensive_register = driver.find_element_by_xpath(reg_path)
-intensive_register.click()
+reg_name = 'DIVI-Intensivregister_' + date
+reg_xpath = '//a[contains(@aria-label,' + "'" + reg_name + "'" + ' )]'
 
-url = driver.current_url
-urlretrieve(url, register_path + '\Intensiveregister_2021-01-01.csv')
-#end_link = driver.find_element_by_xpath('//*[@title="Ende"]')
-#dummy_scroll = driver.find_element_by_link_text('Mitglied werden')
-#actions = ActionChains(driver)
-#actions.move_to_element(start_link).perform()
-#dummy_scroll = driver.find_element_by_link_text('Mitglied werden')
 
-#start_link.click()
+def find(name, path):
+    my_reg_name = name
+    my_reg_xpath = path
+    my_intensive_register = ""
+    try:
+        my_intensive_register = driver.find_element_by_xpath(my_reg_xpath)
+    except:
+        print("DIVI file not found:" + my_reg_name)
+    return my_intensive_register
+
+
+def click(button):
+    intensive_register = button
+    try:
+        intensive_register.click()
+    except:
+        print("Could not click " + reg_name)
+
+
+def save():
+    try:
+        url = driver.current_url
+        urlretrieve(url, register_path + '\Intensiveregister_' + date + '.csv')
+    except:
+        print("Could not save file: " + reg_name)
+
+driver.quit()
+# end_link = driver.find_element_by_xpath('//*[@title="Ende"]')
+# dummy_scroll = driver.find_element_by_link_text('Mitglied werden')
+# actions = ActionChains(driver)
+# actions.move_to_element(start_link).perform()
+# dummy_scroll = driver.find_element_by_link_text('Mitglied werden')
+
+# start_link.click()
 
 '''
 url = 'https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv/divi-intensivregister-2020-12-20-12-15/viewdocument/5328'
